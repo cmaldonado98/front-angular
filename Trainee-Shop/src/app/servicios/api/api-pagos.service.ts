f9fd418362e494b3d75a4b59ddad9f5441eec650
Carlos Maldonado <carlos.maldonado1@cloud.uautonoma.cl>
2021-09-20 23:08:30 -0500

    V1

--- Trainee-Shop/src/app/servicios/api/api-pagos.service.ts
+++ Trainee-Shop/src/app/servicios/api/api-pagos.service.ts
@@ -11,7 +11,8 @@ export class ApiPagosServicio {
 
   url:string = "http://18.220.62.211:5000/users?token="+"000"+"-";
   url2:string = "http://18.220.62.211:5000/pays?token="+"000"+"-";
-  
+
+
   constructor(
     private _http:HttpClient
     ) { }
