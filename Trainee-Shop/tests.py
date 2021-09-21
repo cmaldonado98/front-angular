10ec596788aae17fd32860b2c37c91c1bb45783a
Carlos Maldonado <carlos.maldonado1@cloud.uautonoma.cl>
2021-09-20 23:27:03 -0500

    V3

--- Trainee-Shop/tests.py
+++ Trainee-Shop/tests.py
@@ -145,7 +145,7 @@ for i in range(2,3):
             time.sleep(1)
     time.sleep(2)
 #'''
-driver.get('http://a1a17f2828c0f4d7f83ff926804b9a74-1091942866.us-west-1.elb.amazonaws.com')
+driver.get('http://a2b8287941dd54611907bf97811f50b7-532727409.us-west-1.elb.amazonaws.com')
 nombres = wb.get_sheet_by_name("test")
 wb.close()
 for i in range(2,3):
